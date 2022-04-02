2225c07c65a61505a907ffe81481652b98d1afef
wujieli0207 <wujieli0207@outlook.com>
2022-04-02 08:50:16 +0800

    解决第 83 题

---
+++ 83.删除排序链表中的重复元素.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=typescript
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  let current = head;
+
+  while (current && current.next) {
+    // 如果两个元素相等，把当前指针的下一个节点往后移动一格，否则当前指针向后移动一格即可
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return head;
+}
+// @lc code=end
