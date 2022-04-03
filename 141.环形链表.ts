caad02579546f3f41bb8d674a11fd655d8bf1299
wujieli0207 <wujieli0207@outlook.com>
2022-04-03 14:00:06 +0800

    第 141 题新增快慢指针方法

--- 141.环形链表.ts
+++ 141.环形链表.ts
@@ -18,20 +18,35 @@
  */
 
 function hasCycle(head: ListNode | null): boolean {
-  // 使用 map 保存每一个遍历过的节点
-  let map = new Map();
+  // // 使用 map 保存每一个遍历过的节点
+  // let map = new Map();
 
-  let current = head;
+  // let current = head;
 
-  while (current && current.next) {
-    if (map.has(current.next)) {
-      return true;
-    }
+  // while (current && current.next) {
+  //   if (map.has(current.next)) {
+  //     return true;
+  //   }
 
-    map.set(current, current.val);
-    current = current.next;
+  //   map.set(current, current.val);
+  //   current = current.next;
+  // }
+
+  // return false;
+
+  if (!head || !head.next) return false;
+
+  let slow = head; // 定义慢指针，一次移动一步
+  let fast = head.next; // 定义快指针，一次移动两步
+
+  while (fast !== slow) {
+    // 如果快指针及其 next 指向为空，说明不是环形链表
+    if (!fast || !fast.next) return false;
+
+    slow = slow.next;
+    fast = fast.next.next;
   }
 
-  return false;
+  return true;
 }
 // @lc code=end
