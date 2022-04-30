e589b30ef778b6b2eb1d7bd0b0f185506a80d303
wujieli0207 <wujieli0207@outlook.com>
2022-05-01 07:45:04 +0800

    解决第 35 题

---
+++ 35.搜索插入位置.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=typescript
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+function searchInsert(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor(left + (right - left) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = right - 1;
+    }
+  }
+
+  return left;
+}
+// @lc code=end
